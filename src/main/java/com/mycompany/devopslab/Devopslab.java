ed155fe5f5a4463babb37da15afb3f053c78f36b
cprpocapp <cprsheet@gmail.com>
2026-04-04 10:53:52 +0530

    initial commit

---
+++ src/main/java/com/mycompany/devopslab/Devopslab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devopslab;
+
+/**
+ *
+ * @author Dell
+ */
+public class Devopslab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
